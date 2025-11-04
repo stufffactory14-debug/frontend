cea106b6f7c32231adf44c57e24ec10deaa8a441
Bhavya Gupta(FD-18) <stufffactory14@gmail.com>
2025-11-04 16:30:02 +0530

    Create script.js

---
+++ html/class/dom_js/script.js
@@ -0,0 +1,7 @@
+        const heading = document.getElementById("title");
+        const button = document.getElementById("changeBtn");
+
+       
+        button.addEventListener("click", function() {
+            heading.textContent = "Hello, JavaScript!";
+        });
